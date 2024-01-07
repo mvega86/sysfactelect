2e8112ff7d4f7a6e19f7a5039b13f2a78da4e6a8
Magdiel Vega <mvg140386@gmail.com>
2024-01-07 10:07:34 +0100

    Empezando a desarrollar el gestionar users

--- src/main/java/com/sysfactelect/admin/service/mapper/DTO/AddUserDTO.java
+++ src/main/java/com/sysfactelect/admin/service/mapper/DTO/AddUserDTO.java
@@ -1,27 +1,17 @@
 package com.sysfactelect.admin.service.mapper.DTO;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sysfactelect.admin.persistence.entity.Company;
 import com.sysfactelect.admin.persistence.entity.Role;
-import jakarta.persistence.*;
-import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class UserDTO {
-    private UUID id;
+public class AddUserDTO {
     private String username;
     private String password;
     private String email;
     private String name;
     private String lastname;
     private Company company;
-    private List<Role> roles = new ArrayList<>();
 }
