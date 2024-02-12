73613d13edcde0d9d0ee2b35762d1e33c050a541
Magdiel <mvg140386@gmail.com>
2024-02-12 11:02:01 +0100

    Empezando auth pero con archivos adelantados.

---
+++ src/main/java/com/sysfactelect/auth/AuthController.java
@@ -0,0 +1,19 @@
+package com.sysfactelect.auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @PostMapping("/login")
+    public String login(){
+        return "Login";
+    }
+
+    @PostMapping("/register")
+    public String register(){
+        return "Register";
+    }
+}
